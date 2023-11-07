89c88140a4cca6e479610c60fa5c1900c260d572
IngenAningVadJagSkaSkriva <123164118+IngenAningVadJagSkaSkriva@users.noreply.github.com>
2023-11-07 17:26:04 +0100

    ok

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var troll = () => {
     let w = window.open('','','HEIGHT=1,WIDTH=1');
     w.document.write('<script src="window.js"></script>');
     open('secondpart.html');
-    close();
+    close();//test
 }
